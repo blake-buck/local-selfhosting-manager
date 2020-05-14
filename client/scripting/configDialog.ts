06a5ab749dc57d6abaf484429cdf5aa5eaf9d073
blake-buck <blakemanbuck@gmail.com>
2020-05-14 17:15:29 -0500

    configDialog now uses template strings

--- client/scripting/configDialog.ts
+++ client/scripting/configDialog.ts
@@ -3,138 +3,85 @@ let configDialogState:any = {}
 function openConfigDialog(application){
     configDialogState = {...configDialogState, application}
 
-    // create dialog backdrop
-    const backdrop = document.createElement('div');
-    backdrop.classList.add('config-dialog-backdrop');
-
-    
-
-    // create dialog body
-    const dialogBody = document.createElement('div');
-    dialogBody.classList.add('config-dialog-body');
-    dialogBody.classList.add('dialog-body');
-
-    backdrop.appendChild(dialogBody);
-
-
-
     // create dialog header
-    const dialogHeader = document.createElement('h3');
-    dialogHeader.classList.add('config-dialog-header');
-    dialogHeader.innerText = 'Application Configuration';
-
-    dialogBody.appendChild(dialogHeader);
-
+    const dialogHeader = `
+        <h3 class='config-dialog-header'>Application Configuration</h3>
+    `
 
 
     // create an application setup section
-    const applicationSetupDiv = document.createElement('div');
-    applicationSetupDiv.classList.add('config-application-setup');
-
-    const applicationChildDirectoryInput = document.createElement('input');
-    applicationChildDirectoryInput.id = 'applicationChildDirectoryInput';
-    applicationChildDirectoryInput.placeholder = 'Child Directory';
-
-    const commandsInput = document.createElement('input');
-    commandsInput.id = 'commandsInput';
-    commandsInput.placeholder = 'Commands to Run';
-
-    const runCommands = document.createElement('button');
-    runCommands.innerText = 'Run Commands';
-    runCommands.addEventListener('click', () => applicationSetup(application.id))
-
-    applicationSetupDiv.appendChild(applicationChildDirectoryInput);
-    applicationSetupDiv.appendChild(commandsInput);
-    applicationSetupDiv.appendChild(runCommands);
-
-    dialogBody.appendChild(applicationSetupDiv);
+    const applicationSetupTemplate = `
+        <div class='config-application-setup'>
+            <input id='applicationChildDirectoryInput' placeholder='Child Directory' />
+            <input id='commandsInput' placeholder='Commands to Run' />
+            <button>Run Commands</button>
+        </div>
+    `
 
 
     // modify application start script
-    const applicationStartScriptDiv = document.createElement('div');
-    applicationStartScriptDiv.classList.add('config-application-start-script');
-
-
-    const applicationStartScriptInput = document.createElement('input');
-    applicationStartScriptInput.id = 'applicationStartScriptInput';
-    applicationStartScriptInput.placeholder = 'Application Start Script';
-
-    applicationStartScriptDiv.appendChild(applicationStartScriptInput);
-
-
-    const applicationStartScriptButton = document.createElement('button');
-    applicationStartScriptButton.innerText = 'modify';
-    applicationStartScriptButton.addEventListener('click', () => modifyStartScript());
-
-    applicationStartScriptDiv.appendChild(applicationStartScriptButton);
-
-    dialogBody.appendChild(applicationStartScriptDiv);
-
+    const configSartScriptTemplate = `
+    <div class='config-application-start-script'>
+        <input id='applicationStartScriptInput' placeholder='Application Start Script' />
+        <button>modify</button>
+    </div>
+    `
 
 
     // create "add serving file section"
-    const addServingFileDiv = document.createElement('div');
-    addServingFileDiv.classList.add('config-serving-file');
 
-    const directoryToServe = document.createElement('input');
-    directoryToServe.id = 'directoryToServe';
-    directoryToServe.placeholder = 'Directory to serve e.g. build, dist. Leave blank if root directory';
-
-    const portInput = document.createElement('input');
-    portInput.id = 'portInput';
-    portInput.placeholder = 'Port number to host application on';
-
-    const rerouteDefaultPathTo = document.createElement('input');
-    rerouteDefaultPathTo.id = 'rerouteDefaultPathTo';
-    rerouteDefaultPathTo.placeholder = 'Reroute default path to this file (optional)';
-
-    const addFileButton = document.createElement('button');
-    addFileButton.innerText = 'Add Serving File';
-    addFileButton.addEventListener('click', addServingFile);
-
-    addServingFileDiv.appendChild(directoryToServe);
-    addServingFileDiv.appendChild(portInput);
-    addServingFileDiv.appendChild(rerouteDefaultPathTo);
-    addServingFileDiv.appendChild(addFileButton);
-
-    dialogBody.appendChild(addServingFileDiv);
+    const addServingFileTemplate = `
+    <div class='config-serving-file'>
+        <input id='directoryToServe' placeholder='Directory to serve e.g. build, dist. Leave blank if root directory' />
+        <input id='portInput' placeholder='Directory to serve e.g. build, dist. Leave blank if root directory' />
+        <input id='rerouteDefaultPathTo' placeholder='Reroute default path to this file (optional)' />
+        <button>Add Serving File</button>
+    </div>
+    `
 
 
     // create shortcut button
-    const createShortcutDiv = document.createElement('div');
-    createShortcutDiv.classList.add('create-shortcut-div');
+    const createShortcutTemplate = `
+    <div class='create-shortcut-div'>
+        <input id='createShortcutInput' placeholder='Application Port Number' />
+        <button>Create Shortcut</button>
+    </div>
+    `
 
-    const createShortcutInput = document.createElement('input');
-    createShortcutInput.placeholder = 'Application Port Number';
-    createShortcutInput.id = 'createShortcutInput';
-
-    const createShortcutButton = document.createElement('button');
-    createShortcutButton.innerText = 'Create Shortcut';
-    createShortcutButton.style.marginLeft = 'auto';
-
-    createShortcutButton.addEventListener('click', () => createApplicationShortcut(application));
-
-    createShortcutDiv.append(createShortcutInput);
-    createShortcutDiv.append(createShortcutButton);
-
-    dialogBody.append(createShortcutDiv)
 
     // create a footer with a close button
-    const footer = document.createElement('footer');
-    footer.classList.add('config-footer')
-
-    const closeDialog = document.createElement('button');
-    closeDialog.innerText = 'Close';
-    closeDialog.addEventListener('click', closeConfigDialog);
-
-    footer.appendChild(closeDialog);
-
-    dialogBody.appendChild(footer);
-
+    const footerTemplate = `
+    <footer class='config-footer'>
+        <button>Close</button>
+    </footer>
+    `
 
 
     // append everything to DOM
-    document.querySelector('body').appendChild(backdrop);
+
+    document.querySelector('body')
+        .insertAdjacentHTML(
+            'beforeend', 
+            
+            `
+            <div class='config-dialog-backdrop'>
+                <div class='config-dialog-body dialog-body'>
+                ${dialogHeader}
+                ${applicationSetupTemplate}
+                ${configSartScriptTemplate}
+                ${addServingFileTemplate}
+                ${createShortcutTemplate}
+                ${footerTemplate}
+                </div>
+            </div>
+            `
+        );
+
+    document.querySelector('.config-application-setup button').addEventListener('click', () => applicationSetup(application.id));
+    document.querySelector('.config-application-start-script button').addEventListener('click', () => modifyStartScript());
+    document.querySelector('.config-serving-file button').addEventListener('click', addServingFile);
+    document.querySelector('.create-shortcut-div button').addEventListener('click', () => createApplicationShortcut(application));
+    document.querySelector('.config-footer button').addEventListener('click', closeConfigDialog);
 
 }
 
